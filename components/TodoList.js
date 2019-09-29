9853b77a6d067a20aaabcae65c093db71fc33631
Sony <sonypark0204@gmail.com>
2019-09-30 00:58:11 +0900

    Add, Delete, Toggle 버튼 추가

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {StyleSheet, ScrollView, Text} from 'react-native';
+import {StyleSheet, ScrollView} from 'react-native';
 import TodoListItem from './TodoListItem';
 
-const TodoList = () => {
+const TodoList = ({todos, onRemove, onToggle}) => {
   return (
     <ScrollView contentContainerStyle={styles.listContainer}>
-      <TodoListItem />
+      {todos.map(todo => (
+        <TodoListItem
+          key={todo.id}
+          {...todo}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
     </ScrollView>
   );
 };
--- components/TodoListItem.js
+++ components/TodoListItem.js
@@ -2,19 +2,27 @@ import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 
-const TodoListItem = () => {
+const TodoListItem = ({textValue, id, checked, onRemove, onToggle}) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
-        <View style={styles.completeCircle}>
-          <Icon name="circledowno" size={30} color="#3143e8" />
-        </View>
+      <TouchableOpacity onPressOut={onToggle(id)}>
+        {checked ? (
+          <View style={styles.completeCircle}>
+            <Icon name="circledowno" size={30} color="#3143e8" />
+          </View>
+        ) : (
+          <View style={styles.circle} />
+        )}
       </TouchableOpacity>
-      <Text style={[styles.text, styles.strikeText]}>
-        Items will be shown here
+      <Text
+        style={[
+          styles.text,
+          checked ? styles.strikeText : styles.unstrikeText,
+        ]}>
+        {textValue}
       </Text>
       <TouchableOpacity style={styles.buttonContainer}>
-        <Text style={styles.buttonText}>
+        <Text style={styles.buttonText} onPress={onRemove(id)}>
           <Icon name="delete" size={30} color="#e33057" />
         </Text>
       </TouchableOpacity>
